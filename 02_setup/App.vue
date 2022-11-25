823e96b4274f75264fa90b5f7fb51036338daa05
veronicaJun <87801036@qq.com>
2022-11-25 15:05:03 +0800

    ✨ feat: 02_setup

---
+++ 02_setup/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>信息</h1>
+  <h2>{{name}}</h2>
+  <h2>{{age}}</h2>
+  <button @click="sayHello">说话</button>
+</template>
+
+<script>
+// import {h} from 'vue';
+export default {
+  name: 'App',
+  setup(){
+    let name = '张三'
+    let age = 18
+
+    function sayHello(){
+      console.log(`我叫${name},我${age}岁,你好呀`)
+    }
+    // 返回一个对象(常用)
+    return {
+      name,
+      age,
+      sayHello
+    }
+
+    // 返回一个函数(渲染函数)
+    // return ()=> h('h1','娃哈哈')
+  }
+}
+</script>
+
