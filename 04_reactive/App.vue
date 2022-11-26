ac8b5635362336060ee428bdd4f710c3f3fcc6d9
veronicaJun <87801036@qq.com>
2022-11-26 15:29:08 +0800

    ✨ feat: 04_reactive

---
+++ 04_reactive/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <h1>信息</h1>
+  <h2>姓名:{{person.name}}</h2>
+  <h2>年龄:{{person.age}}</h2>
+  <h3>工作:{{person.job.type}}</h3>
+  <h3>薪水:{{person.job.salary}}</h3>
+  <button @click="changeInfo">修改信息</button>
+</template>
+
+<script>
+import {ref, reactive} from 'vue';
+export default {
+  name: 'App',
+  setup(){
+    // 普通的数据
+    // let name = '张三'
+    // let age = 18
+
+    // 响应式数据
+    // let name = ref('张三')
+    // let age = ref(18)
+    // let job = ref({
+    //   type: '前端工程师',
+    //   salary: '30k'
+    // })
+
+    // reactive
+    // let job = reactive({
+    //   type: '前端工程师',
+    //   salary: '30k'
+    // })
+    // let hobby = reactive(['1','2','3'])
+
+    let person = reactive({
+      name : '张三',
+      age : 18,
+      job : {
+        type: '前端工程师',
+        salary: '30k'
+      },
+      hobby : ['1','2','3']
+    })
+
+    function changeInfo() {
+      // name.value = '李思'
+      // age.value= 28
+
+      // ref
+      // job.value.type = '后端工程师'
+      // job.value.salary = '50k'
+
+      //reactive
+      // console.log(job);
+      // job.type = '后端工程师'
+      // job.salary = '50k'
+
+      // hobby[0] = 999;
+      // console.log(hobby);
+
+      //reactive
+      console.log(person)
+    }
+
+    return {
+      person,
+      changeInfo
+    }
+
+  }
+}
+</script>
+
--- README.md
+++ README.md
@@ -80,10 +80,17 @@ npm run dev
 
 - 作用: 定义一个响应式的数据
 - 语法: ```const xxx = ref(initValue)``` 
-  - 创建一个包含响应式数据的<strong style="color:#DD5145">引用对象（reference对象，简称ref对象）</strong>。
+  - 创建一个包含响应式数据的**引用对象（reference对象，简称ref对象）**。
   - JS中操作数据： ```xxx.value```
   - 模板中读取数据: 不需要.value，直接：```<div>{{xxx}}</div>```
 - 备注：
   - 接收的数据可以是：基本类型、也可以是对象类型。
   - 基本类型的数据：响应式依然是靠``Object.defineProperty()``的```get```与```set```完成的。
   - 对象类型的数据：内部 <i style="color:gray;font-weight:bold">“ 求助 ”</i> 了Vue3.0中的一个新函数—— ```reactive```函数。(reactive-->proxy)
+  
+## 3.reactive函数
+
+- 作用: 定义一个**对象类型**的响应式数据（基本类型不要用它，要用```ref```函数）
+- 语法：```const 代理对象= reactive(源对象)```接收一个对象（或数组），返回一个**代理对象（Proxy的实例对象，简称proxy对象）**
+- reactive定义的响应式数据是“深层次的”。
+- 内部基于 ES6 的 Proxy 实现，通过代理对象操作源对象内部数据进行操作。
